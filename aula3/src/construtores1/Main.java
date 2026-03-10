74a7e8d5f598b24983051e95bef109a938ba16e0
JoaoRB163 <joaobataglion087@gmail.com>
2026-03-09 21:29:07 -0300

    contrutor com herança

--- aula3/src/construtores1/Main.java
+++ aula3/src/construtores1/Main.java
@@ -2,17 +2,20 @@ package construtores1;
 
 public class Main {
     public static void main(String[] args) {
-        
-        Produtos();
 
-        // Pessoas();
+        // Produtos();
+
+        Pessoas();
 
     }
 
     public static void Pessoas(){
-        Pessoa p = new Pessoa("123.456.789-00", "Gerivaldo");
+        
+        Pessoa pessoa1 = new Pessoa("123.456.789-00", "Gerivaldo");
+        Funcionario pessoa2 = new Funcionario("009.876.543-21", "Gilberto", 2890.6);
+        System.out.println("Pessoa: " + pessoa1.getNome() + "\nCPF: " + pessoa1.getCpf());
+        System.out.println("Pessoa: " + pessoa2.getNome() + "\nCPF: " + pessoa2.getCpf() + "\nSalario: R$" + pessoa2.getSalario());
 
-        System.out.println("Pessoa: " + p.getNome() + "\nCPF: " + p.getCpf());
     }
     
     public static void Produtos(){
