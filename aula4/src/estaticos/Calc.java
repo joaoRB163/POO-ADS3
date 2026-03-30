7bc1f32f6151d675bdf80dbff0a723bfb002046c
joaoRB163 <joaobataglion087@gmail.com>
2026-03-30 18:52:37 -0300

    mais possíbilidades com métodos estáticos

--- aula4/src/estaticos/Calc.java
+++ aula4/src/estaticos/Calc.java
@@ -5,4 +5,21 @@ public class Calc {
     public static double somar (double a, double b){
         return (a + b);
     }
+
+    public static double subtrair (double a, double b){
+        return (a - b);
+    }
+
+    public static double multiplicar (double a, double b){
+        return (a * b);
+    }
+
+    public static double dividir (double a, double b){
+        if(b != 0){
+            return (a / b);
+        } else {
+            System.err.println("Impossível dividir um número por 0");
+            return Double.NaN;
+        }
+    }
 }
--- aula4/src/estaticos/Main.java
+++ aula4/src/estaticos/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         
         double x = Calc.somar(3.5, 7);
-        
         System.out.println(x);
+
+        System.out.println(Calc.subtrair(5, 3));
+        System.out.println(Calc.multiplicar(12, 2));
+        System.out.println(Calc.dividir(30, 6));
     }
 }
