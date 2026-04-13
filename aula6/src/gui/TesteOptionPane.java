d031ed9480063b1f9f01f32b98bc1ad07161233a
JoaoRB163 <joaobataglion087@gmail.com>
2026-04-13 19:03:09 -0300

    opções em JO

--- aula6/src/gui/TesteOptionPane.java
+++ aula6/src/gui/TesteOptionPane.java
@@ -6,10 +6,13 @@ public class TesteOptionPane {
     public static void main(String[] args) {
         double a, b;
         double c = 0;
+        String[] opc = {"a", "b", "c"};
         
         a = Double.valueOf(JOptionPane.showInputDialog("Digite um valor para A"));
         b = Double.valueOf(JOptionPane.showInputDialog("Digite um valor para B"));
 
+        JOptionPane.showOptionDialog(null, "Escolha", "Titulo", 0, 0, null, opc, opc[0]);
+        
         if(b != 0){
             c = a / b;
             JOptionPane.showMessageDialog(null, "A / B = " + c); 
